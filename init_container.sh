31dcb72cf2a59bba9b9a43c97f6fef828a6f0360
Susumu Saito <susumu@pcl.cs.waseda.ac.jp>
2018-07-12 06:28:51 +0000

    securely going to public

--- init_container.sh
+++ init_container.sh
@@ -1,5 +1,21 @@
-#mysql -u root -proot -Bse "CREATE USER $MYSQL_USERNAME@'myadmin.dcnet' IDENTIFIED BY '$MYSQL_PASSWORD';"
-sudo sed -i "s/bind-address/#bind-address/" /etc/mysql/mysql.conf.d/mysqld.conf;
+# mysql
+if [ -n "${MYSQL_USERNAME}" ] && [ -n "${MYSQL_PASSWORD}" ]; then
+  mysql -u root -proot -Bse "CREATE USER $MYSQL_USERNAME@'myadmin.dcnet' IDENTIFIED BY '$MYSQL_PASSWORD';"
+  mysql -u root -proot -Bse "GRANT ALL PRIVILEGES ON *.* TO $MYSQL_USERNAME@'myadmin.dcnet'"
+fi
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/mysql.conf.d/mysqld.cnf;
 service mysql restart;
 
+# settings/global.json
+if [[ -n "${BASE_URL}" ]]; then
+  sed -i "s/\"BaseUrl.*/\"BaseUrl\"\: \"$BASE_URL\",/" /root/DynamicCrowd/settings/global.json;
+fi
+if [[ -n "${AWS_ACCESS_KEY_ID}" ]]; then
+  sed -i "s/\"AWSAccessKeyId.*/\"AWSAccessKeyId\"\: \"$AWS_ACCESS_KEY_ID\",/" /root/DynamicCrowd/settings/global.json;
+fi
+if [[ -n "${AWS_SECRET_ACCESS_KEY}" ]]; then
+  sed -i "s/\"AWSSecretAccessKey.*/\"AWSSecretAccessKey\"\: \"$AWS_SECRET_ACCESS_KEY\"/" /root/DynamicCrowd/settings/global.json;
+fi
+
+# database migration
 cd ~/DynamicCrowd; python manage.py makemigrations;
